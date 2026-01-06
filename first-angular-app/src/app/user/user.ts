285039be75fa8f36454918afc9e20d72d81bfba8
SunilProjectsRepo <sunilpatrocse@gmail.com>
2026-01-06 01:18:07 -0500

    First Angular Project

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,6 +11,33 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex]
+  //This is a variable, no need to declare it with let or const. It is a public variable. 
+  // If we want we can mark it as private variable "private selectedUser = DUMMY_USERS[randomIndex];"
+  // private variable will be available within the class
+  //selectedUser = DUMMY_USERS[randomIndex]  
+  
+  //Using Signal
+  selectedUser = signal(DUMMY_USERS[randomIndex]) 
+  
+  //a function inside of a class
+  // used like a property
+  // with this keyword we can access the class and get other properties of the class
+  //get imagePath() {
+  //  return 'assets/users/' + this.selectedUser.avatar;
+  //} 
+
+  //Using Signal
+  imagePath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  }
+  );
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //this.selectedUser = DUMMY_USERS[randomIndex]
+    //using signal instead of above line
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+
 
 }
